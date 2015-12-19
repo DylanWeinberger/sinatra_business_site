d66bf85841d2baffd46119ef22bd319b987466b5
Dylan Weinberger <djweinbe@gmail.com>
2015-12-19 17:52:35 -0500

    made some changes to the css and the images displayed on all erb files. made an interactive xwing for all pages.

--- public/js/home.js
+++ public/js/home.js
@@ -5,6 +5,28 @@ $('.light-box').css({
 	'opacity':'0'
 });
 
+$(document).keydown(function(){
+		if (event.keyCode == 68) {
+			$(".xwing").animate({
+				"left": "+=240px"
+			}, 10);
+		}else if (event.keyCode == 65) {
+			$(".xwing").animate({
+				"left": "-=240px"
+			}, 10);
+		}else if  (event.keyCode == 87) {
+			$(".xwing").animate({
+				"top": "-=240px"
+			}, 10);	
+		}else if (event.keyCode == 83) {
+			$(".xwing").animate({
+				"top": "+=240px"
+			}, 10);
+		}	else{
+
+		}
+	});
+
 $('body').mouseover(function(){
 	$(".light-box").css({
 		"opacity": '1'
@@ -16,7 +38,10 @@ $('body').click(function(){
 	$('.forget').fadeIn(2000);
 	$('.max').fadeIn(5000);
 	$('.snootles').fadeIn(10000);
-
+	// $('.xwing').animate({left: "+=1800"}, 9000);
+	// $
+	// I was going to make an x-wing fly in slowly from off the screen when the user clicks on the page. But i decided instead
+	// to allow the user to control the xwing with wasd 
 	// ({
 	// 	"opacity": ".7"
 	// });
