fb6cb7104a5cdd7c1000671a7355fde29bfa062a
Dylan Weinberger <djweinbe@gmail.com>
2015-12-20 12:18:55 -0500

    attached new banner and footer to all pages except menu. made sure layout of all pages are neat. added jabba to the contact page.

--- public/js/home.js
+++ public/js/home.js
@@ -60,6 +60,28 @@ $(".falcon").click(function(){
 		}, 8000);
 	});
 
+$('.hutt').click(function(){
+	$('.hutt').css ({
+		"width": "150%"
+	});
+});
+
+
+    var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', 'images/Jabba_the_Hutt-Laugh.mp3');
+        // audioElement.setAttribute('autoplay', 'autoplay');
+        //audioElement.load()
+
+        $.get();
+
+        audioElement.addEventListener("load", function() {
+            audioElement.play();
+        }, true);
+
+        $('.hutt').click(function() {
+            audioElement.play();
+        });
+
 
 
 
